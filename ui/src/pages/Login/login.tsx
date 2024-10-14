94d9fb8c6c33dc905d0eef5e7ea7c7c942a3261d
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2024-10-14 17:37:51 -0600

    Add Icon types.

--- ui/src/pages/Login/login.tsx
+++ ui/src/pages/Login/login.tsx
@@ -12,6 +12,7 @@ import {
 } from "@nextui-org/react";
 import { EyeFilledIcon } from "./EyeFilledIcon.tsx";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcont.tsx";
+import { SearchIcon } from "@/components/icons.tsx";
 import DefaultLayout from "@/layouts/default";
 
 /*
@@ -30,7 +31,39 @@ export default function Login() {
 
   return (
     <DefaultLayout>
-      <Card className="max-w-[400px]">
+      <div className="w-[340px] h-[240px] px-8 rounded-2xl flex justify-center items-center bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+        <Input
+          label="Search"
+          isClearable
+          radius="lg"
+          classNames={{
+            label: "text-black/50 dark:text-white/90",
+            input: [
+              "bg-transparent",
+              "text-black/90 dark:text-white/90",
+              "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+            ],
+            innerWrapper: "bg-transparent",
+            inputWrapper: [
+              "shadow-xl",
+              "bg-default-200/50",
+              "dark:bg-default/60",
+              "backdrop-blur-xl",
+              "backdrop-saturate-200",
+              "hover:bg-default-200/70",
+              "dark:hover:bg-default/70",
+              "group-data-[focus=true]:bg-default-200/50",
+              "dark:group-data-[focus=true]:bg-default/60",
+              "!cursor-text",
+            ],
+          }}
+          placeholder="Type to search..."
+          startContent={
+            <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+          }
+        />
+      </div>
+      <Card className="max-w-[400px] mx-auto">
         <CardHeader className="flex gap-3">
           <Image
             alt="nextui logo"

1926e43372a80b822e256174e5b25ee42a5bc75c
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2024-10-20 13:49:19 -0600

    Login with style, Formik and Yup validation working.

--- frontend/src/pages/Login/login.tsx
+++ frontend/src/pages/Login/login.tsx
@@ -6,24 +6,19 @@ import {
   Divider,
   CardBody,
   Input,
-  Image,
   Button,
   ButtonGroup,
 } from "@nextui-org/react";
-import { Form, Field, Formik } from "formik";
+import { Form, Formik } from "formik";
 import * as Yup from "yup";
 
+import DefaultLayout from "@/layouts/default";
 import { EyeFilledIcon } from "./EyeFilledIcon.tsx";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon.tsx";
-import DefaultLayout from "@/layouts/default";
 
 interface LoginValues {
   email: string;
   password: string;
-}
-
-interface LoginOther {
-  message: string;
 }
 
 export const Login: React.FC<{}> = () => {
@@ -52,7 +47,6 @@ export const Login: React.FC<{}> = () => {
     ],
   };
 
-  // Form and validation
   const validationSchema: Yup.ObjectSchema<LoginValues> = Yup.object().shape({
     email: Yup.string().email("Invalid email.").required("Required"),
     password: Yup.string().required("Pasword required"),
@@ -66,7 +60,6 @@ export const Login: React.FC<{}> = () => {
     <DefaultLayout>
       <Card className="max-w-[350px] mx-auto px-8 rounded-2xl flex justify-center items-center bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
         <CardHeader className="flex gap-3 justify-center items-center">
-          <Image alt="nextui logo" height={40} radius="sm" src="" width={40} />
           <div className="flex flex-col">
             <p className="text-md">Democracia</p>
             <p className="text-small text-default-500">Democracia</p>
@@ -88,62 +81,74 @@ export const Login: React.FC<{}> = () => {
               alert(JSON.stringify(values, null, 2));
             }}
           >
-            <Form>
-              <div className="w-90 m-1 px-8 rounded-2xl flex justify-center items-center">
-                <Input
-                  isRequired
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  isClearable
-                  radius="lg"
-                  classNames={inputStyle}
-                />
-              </div>
-              <div className="w-90 m-1 px-8 rounded-2xl flex justify-center items-center">
-                <Input
-                  isRequired
-                  id="password"
-                  name="password"
-                  placeholder="Pasword"
-                  radius="lg"
-                  classNames={inputStyle}
-                  endContent={
-                    <button
-                      className="focus:outline-none"
-                      type="button"
-                      onClick={setPassVisibility}
-                      aria-label="toggle password visibility"
-                    >
-                      {isPassVisible ? (
-                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      ) : (
-                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                      )}
-                    </button>
-                  }
-                  type={isPassVisible ? "text" : "password"}
-                  className="max-w-xs"
-                />
-              </div>
+            {(props) => (
+              <Form onSubmit={props.handleSubmit}>
+                <div className="w-90 m-1 px-8 rounded-2xl flex justify-center items-center">
+                  <Input
+                    isRequired
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                    isClearable
+                    radius="lg"
+                    classNames={inputStyle}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.email}
+                    name="email"
+                  />
+                </div>
+                {props.errors.email && (
+                  <div id="feedback">{props.errors.email}</div>
+                )}
 
-              <div className="flex justify-center items-center">
-                <Button
-                  radius="full"
-                  className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
-                  type="submit"
-                >
-                  Login
-                </Button>
-                <Button
-                  radius="full"
-                  className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
-                >
-                  Register
-                </Button>
-              </div>
-            </Form>
+                <div className="w-90 m-1 px-8 rounded-2xl flex justify-center items-center">
+                  <Input
+                    isRequired
+                    id="password"
+                    placeholder="Pasword"
+                    radius="lg"
+                    classNames={inputStyle}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.password}
+                    name="password"
+                    endContent={
+                      <button
+                        className="focus:outline-none"
+                        type="button"
+                        onClick={setPassVisibility}
+                        aria-label="toggle password visibility"
+                      >
+                        {isPassVisible ? (
+                          <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        ) : (
+                          <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                        )}
+                      </button>
+                    }
+                    type={isPassVisible ? "text" : "password"}
+                    className="max-w-xs"
+                  />
+                </div>
+
+                <div className="flex justify-center items-center my-2">
+                  <Button
+                    radius="full"
+                    className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    radius="full"
+                    className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+                  >
+                    Register
+                  </Button>
+                </div>
+              </Form>
+            )}
           </Formik>
         </CardBody>
 
@@ -151,9 +156,15 @@ export const Login: React.FC<{}> = () => {
 
         <CardFooter>
           <ButtonGroup>
-            <Button>Google</Button>
-            <Button>Facebook</Button>
-            <Button>Phone</Button>
+            <Button color="primary" variant="flat">
+              Google
+            </Button>
+            <Button color="primary" variant="flat">
+              Facebook
+            </Button>
+            <Button color="primary" variant="flat">
+              Phone
+            </Button>
           </ButtonGroup>
         </CardFooter>
       </Card>
